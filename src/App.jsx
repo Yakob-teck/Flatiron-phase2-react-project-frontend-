d2cff50ed54fd03d46072cb82f32bbaf9bad32cb
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-10-01 10:05:14 -0600

    change on app components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import Header from "./components/header";
+import Main from "./components/Main";
 import Footer from "./components/footer";
-function App() {
+const App = () => {
   return (
     <div>
       <Header />
+      <Main />
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
