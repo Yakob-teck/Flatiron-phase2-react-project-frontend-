67a35ff2b78f70fb33dfb662c25bc47552d08109
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-10-03 09:34:52 -0600

    create new form component to post request

---
+++ src/components/videoForm.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const VideoForm = ({ addVideo }) => {
+  const initialFormData = {
+    title: "",
+    url: "",
+    thumbnail: "",
+    duration: "",
+    views: 0,
+    likes: 0,
+    dislikes: 0,
+    description: "",
+  };
+};
