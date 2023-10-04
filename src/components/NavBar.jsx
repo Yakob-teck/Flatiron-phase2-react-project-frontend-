9d1945fbd8ec931d0e1b28dd2a1f2c1f6c691479
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-10-03 20:22:51 -0600

    heade file rename

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const NavBar = () => {
+  const headerStyles = {
+    backgroundColor: "#333",
+    color: "rgb(225, 217, 239)",
+    display: "flex",
+    margintop: "10px",
+    width: "auto",
+    alignItems: "center",
+    padding: "20px",
+  };
+
+  const navStyles = {
+    marginLeft: "auto",
+  };
+
+  const ulStyles = {
+    listStyle: "none",
+    display: "flex",
+    gap: "20px",
+    justifyContent: "center",
+  };
+
+  const liStyles = {
+    margin: "0 10px",
+    textAlign: "center",
+  };
+
+  const linkStyles = {
+    textDecoration: "none",
+    color: "#646cff",
+    fontWeight: "500",
+    transition: "color 0.3s ease-in-out",
+  };
+
+  return (
+    <header style={headerStyles}>
+      <h1>My Video Search</h1>
+      <hr />
+      <nav style={navStyles}>
+        <ul style={ulStyles}>
+          <li style={liStyles}>
+            <a href="#someID1" style={linkStyles}>
+              Home
+            </a>
+          </li>
+          <li style={liStyles}>
+            <a href="#someID2" style={linkStyles}>
+              Videos
+            </a>
+          </li>
+
+          <li style={liStyles}>
+            <a href="#" style={linkStyles}>
+              Contact Us
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
