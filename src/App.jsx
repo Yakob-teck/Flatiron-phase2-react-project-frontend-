07dcc599783865ac12c00b016529c6920056b807
Yakob Asebe <asebeyakob@gmail.com>
2023-10-06 20:05:00 -0600

    import browther router  and client side routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
 import React from "react";
-import Footer from "./components/footer";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; // Import Link
+import Footer from "./components/Footer";
 import Main from "./components/Main";
-import VideosList from "./components/videosList.jsx";
-import NavBar from "./components/navBar";
+import VideosList from "./components/VideosList";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import "./App.css";
+
 const App = () => {
+  const greeting = "Welcome to my website!";
+
   return (
-    <div>
-      <NavBar />
-      <Main />
-      <VideosList />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">
+                <h3>Home</h3>
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">
+                {" "}
+                <h3>About Us</h3>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home greeting={greeting} />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Main />
+        <VideosList />
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
