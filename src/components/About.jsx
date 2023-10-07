622ef20474aff2a53d805423a76a82548278ba4d
Yakob Asebe <asebeyakob@gmail.com>
2023-10-06 20:05:40 -0600

    add about components

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed
+        metus at lectus hendrerit auctor. Proin a augue eget ex vehicula
+        consectetur. Vivamus vel bibendum elit, vel posuere dui.
+      </p>
+      <p>
+        Integer aliquam elit a urna bibendum, sit amet consequat sapien
+        lobortis. Fusce id fringilla ligula, a eleifend justo. Maecenas
+        ullamcorper a erat ac sodales.
+      </p>
+      <p>
+        Nulla auctor lectus non metus hendrerit, sed rhoncus nunc consequat.
+        Praesent sagittis, elit sit amet tincidunt vehicula, libero libero
+        bibendum metus, sit amet dictum enim mi a dui.
+      </p>
+    </div>
+  );
+}
+
+export default About;
